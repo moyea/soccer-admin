d015d74f18b0a7ec61973f10565579f7c8733474
Qinghua <qinghua1994@hotmail.com>
2018-12-03 00:59:47 +0800

    initial project

---
+++ config-overrides.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Qinghua on 2018/12/1 14:45.
+ */
---
+++ src/pages/dashboard/Dashboard.service.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import PropType from 'prop-types';
+
+class Dashboard
+
+.
+service;
+extends
+Component;
+{
+
+    render();
+    {
+        return (
+            <div>
+                $END$
+            </div>
+        );
+    }
+}
+
+export default Dashboard.service;
